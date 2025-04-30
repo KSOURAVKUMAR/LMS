edffd25b08489656f3e78b48d10404d62a3a47ea
KSOURAVKUMAR <162047461+KSOURAVKUMAR@users.noreply.github.com>
2025-04-30 15:48:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: https://lms-frontend-lf9u.onrender.com,
+    origin:  process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(express.urlencoded({extended: true}));
