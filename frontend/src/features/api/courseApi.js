77193494642994942d52a439c25252fb5435de49
KSOURAVKUMAR <162047461+KSOURAVKUMAR@users.noreply.github.com>
2025-04-30 16:05:18 +0530

    Update courseApi.js

--- frontend/src/features/api/courseApi.js
+++ frontend/src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "https://lms-backend-u0e4.onrender.com/api/v1/course";
+const COURSE_API = `${import.meta.env.VITE_BACKEND_URL}/api/v1/course`;
 export const courseApi = createApi({
   reducerPath: "courseApi",
   tagTypes: ["Refetch_Creator_Course", "Refetch_Lecture"],
