ad6b627155176616e32a1f9173e9a8f72bea24f1
KSOURAVKUMAR <162047461+KSOURAVKUMAR@users.noreply.github.com>
2025-04-30 16:17:05 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,12 +4,19 @@ export const generateToken = (res, user, message) => {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
         expiresIn: '1d',
     });
+    // res.cookie('token', token, {
+    //     httpOnly: true,
+    //     // secure: process.env.NODE_ENV === 'production',
+    //     sameSite: 'strict',
+    //     maxAge: 24 * 60 * 60 * 1000, // 1 day
+    // });
     res.cookie('token', token, {
-        httpOnly: true,
-        // secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production', // required for HTTPS
+          sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // required for cross-origin
+          maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
+
     return res.status(200).json({
         success: true,
         message,
