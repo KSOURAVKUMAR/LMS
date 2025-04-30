0609f5ed2aaf9a475c13d2641708d6c13fa10ae1
KSOURAVKUMAR <162047461+KSOURAVKUMAR@users.noreply.github.com>
2025-04-30 16:06:06 +0530

    Update purchaseApi.js

--- frontend/src/features/api/purchaseApi.js
+++ frontend/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "https://lms-backend-u0e4.onrender.com/api/v1/purchase";
+const COURSE_PURCHASE_API = `${import.meta.env.VITE_BACKEND_URL}/api/v1/purchase`;
 
 
 export const purchaseApi = createApi({
