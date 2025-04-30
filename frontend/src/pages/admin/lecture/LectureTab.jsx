cc14399d1fcd2eb462a756367828daeb2ca7e44f
KSOURAVKUMAR <162047461+KSOURAVKUMAR@users.noreply.github.com>
2025-04-30 15:31:32 +0530

    Update LectureTab.jsx

--- frontend/src/pages/admin/lecture/LectureTab.jsx
+++ frontend/src/pages/admin/lecture/LectureTab.jsx
@@ -20,7 +20,7 @@ import {
   useRemoveLectureMutation,
 } from "@/features/api/courseApi";
 import { useNavigate, useParams } from "react-router-dom";
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const MEDIA_API = "https://lms-backend-u0e4.onrender.com/api/v1/media";
 const LectureTab = () => {
   const navigate = useNavigate();
   const [lectureTitle, setLectureTitle] = useState("");
